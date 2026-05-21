f66d26402141db237315a4550f5808dd1f3a6498
Sergio Mamani <mamanisergio88@gmail.com>
2026-05-21 19:22:12 -0300

    feat: initial Spring Boot project setup

---
+++ src/main/java/com/sergio/sie/SieSistemaIntegralInventarioApplication.java
@@ -0,0 +1,13 @@
+package com.sergio.sie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SieSistemaIntegralInventarioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SieSistemaIntegralInventarioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sergio/sie/SieSistemaIntegralInventarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sergio.sie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SieSistemaIntegralInventarioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
